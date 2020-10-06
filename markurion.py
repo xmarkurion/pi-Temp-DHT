10e32fb794c02310a301b235f2e98771ff14feb1
Markurion <xmarkurion@gmail.com>
2020-10-06 12:17:35 +0100

    precent delete

--- markurion.py
+++ markurion.py
@@ -18,7 +18,7 @@ try:
 
         if humidity is not None and temperature is not None:
             #print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-            temp = "{0:.1f}%".format(temperature)
+            temp = "{0:.1f}".format(temperature)
             hum = "{0:.2f}%".format(humidity)
             stringi = "T:" + str(temp) + " H:" + hum
             czas = time.strftime("%H:%M:%S %d/%m")
